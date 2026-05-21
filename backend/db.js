33f215dc686e54eb730b3f80d8fcf0438b5907a5
Joao Vitor <joaovitorfreiredasilva@gmail.com>
2026-05-21 17:10:43 -0300

    Fix SQLite data directory creation for Render

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,16 @@
+const fs = require('fs');
 const knex = require('knex');
 const path = require('path');
+
+const dataDir = path.join(__dirname, 'data');
+if (!fs.existsSync(dataDir)) {
+  fs.mkdirSync(dataDir, { recursive: true });
+}
 
 const db = knex({
   client: 'sqlite3',
   connection: {
-    filename: path.join(__dirname, 'data', 'db.sqlite3')
+    filename: path.join(dataDir, 'db.sqlite3')
   },
   useNullAsDefault: true
 });
